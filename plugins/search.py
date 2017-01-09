40cfd9e19d01926c0ed2ea7ca646c76f34563109
root <root@falkor.linaccess.za.net>
2017-01-09 14:21:35 +0200

    Ansible docs search version 1

---
+++ plugins/search.py
@@ -0,0 +1,12 @@
+from cloudbot import hook
+
+@hook.command("search", "doc")
+def search(text):
+    """ search <text> returns ansible docs url.
+    working to include top results"""
+    res_string = ''
+    for word in text.split():
+        print(word)
+        res_string += word+'+'
+    
+    return 'http://docs.ansible.com/ansible/#stq='+res_string[:-1]

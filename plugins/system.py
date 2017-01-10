563766cef9a0baf5ab84ee69a350d5a934c4420c
John R Barker <john@johnrbarker.com>
2017-01-10 10:00:02 +0000

    Correct link to proposal

--- plugins/system.py
+++ plugins/system.py
@@ -4,4 +4,4 @@ from cloudbot import hook
 @hook.command(autohelp=False)
 def about():
     """Gives information about AnsiBot"""
-    return "AnsiBot comments can be added to https://github.com/ansible/community/issues/48. These will be discussed in the Core meetings."
+    return "AnsiBot comments can be added to https://github.com/ansible/proposals/issues/48. These will be discussed in the Core meetings."

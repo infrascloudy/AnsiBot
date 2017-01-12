6e1d819b140c0fb846f2981e154271da994ddac5
root <root@falkor.linaccess.za.net>
2017-01-12 19:12:33 +0200

    Introducing AnswerBot as the assigned identity

--- plugins/system.py
+++ plugins/system.py
@@ -3,5 +3,5 @@ from cloudbot import hook
 
 @hook.command(autohelp=False)
 def about():
-    """Gives information about AnsiBot"""
-    return "AnsiBot comments can be added to https://github.com/ansible/proposals/issues/48. These will be discussed in the Core meetings."
+    """Gives information about AnswerBot"""
+    return "AnswerBot comments can be added to https://github.com/ansible/proposals/issues/48. These will be discussed in the Core meetings."
